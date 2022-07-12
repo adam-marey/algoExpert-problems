06f629932d6e9c4c33a202eafeae1188dbfc3d39
Sulaiman Marey <adammarey51@gmail.com>
2022-07-11 22:40:01 -0700

    added smallest deference problem python solution

---
+++ Arrays.js/smallest-deference.py
@@ -0,0 +1,42 @@
+
+#Smallest Difference O ^
+#Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute
+#difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the
+#first position.
+#Note that the absolute difference of two integers is the distance between them on the real number line, For example, the
+#absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+#You can assume that there will only be one pair of numbers with the smallest difference.
+
+#Sample Input:
+#arrayOne = [-1, 5, 10, 20, 28, 3]
+#arrayTwo = [26, 134, 135, 15, 17]
+
+#ouput:
+#[28, 26]
+
+def smallestDifference(arrayOne, arrayTwo):
+  arrayOne.sort()
+  arrayTwo.sort()
+  idxOne = 0
+  idxTwo = 0
+  smallest = float("inf")
+  current = float("inf")
+  smallestPair = []
+
+
+  while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+    firstNum = arrayOne[idxOne]
+    secondNum = arrayTwo[idxTwo]
+
+    if firstNum < secondNum:
+      current = secondNum - firstNum
+      idxOne += 1
+    elif secondNum < firstNum:
+      current = firstNum - secondNum
+      idxTwo += 1
+    else:
+      return [firstNum, secondNum]
+    if smallest > current:
+      smallest = current
+      smallestPair = [firstNum, secondNum]
+  return smallestPair
